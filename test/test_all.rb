ad734f34dc650811fa6e916df1a1aeae429c57fe
Richard Smith-Unna <rds45@cam.ac.uk>
2014-06-22 18:29:10 -0400

    fix test runner - now working

--- test/test_all.rb
+++ test/test_all.rb
@@ -3,17 +3,19 @@ require 'json'
 require 'tmpdir'
 
 testdir = File.expand_path(File.dirname(__FILE__))
-scrapers = Dir[File.join(thisdir, '..', '*.json')]
+scraperglob = File.join(testdir, '..', 'scrapers', '*.json')
+scrapers = Dir[scraperglob]
 
-scrapers = 0
+scrapercount = 0
 passed = 0
 warnings = 0
 errors = 0
 
-Dir.mktmpdir do |tmpdir|
-  Dir.chdir(mktmpdir) do
+tmpdir = Dir.mktmpdir
+puts "using tmp dir #{tmpdir}"
+  Dir.chdir(tmpdir) do
     scrapers.each do |scraper|
-      scrapers += 1
+      scrapercount += 1
       basename = File.basename scraper
       puts "scraper #{basename}"
       testobjectpath = File.join(testdir, basename)
@@ -25,47 +27,60 @@ Dir.mktmpdir do |tmpdir|
         # run the scraper
         puts "running quickscrape for URL #{url}"
         results = nil
-        # hash the url
-        hash = Digest::SHA256.hexdigest url
         # run the scraper
         cmd = "quickscrape"
         cmd += " --url #{url}"
-        cmd += " --scraper #{opts.definition}"
-        cmd += " --output #{hash}"
+        cmd += " --scraper #{scraper}"
+        cmd += " --output output"
         cmd += " --loglevel silent"
-        puts `cmd`
+        puts `#{cmd}`
         # load the output
-        unless File.exist? 'results.json'
-          puts "FAIL: no results from scraping"
-          errors += 1
-        end
-        Dir.chdir(hash) do
+        cleanurl = url.gsub(/:?\/+/, '_')
+        Dir.chdir("output/#{cleanurl}") do
+          unless File.exist? 'results.json'
+            puts "FAIL: no results from scraping"
+            errors += 1
+          end
           results = JSON.load(File.open 'results.json')
-          filehashes = `md5sum !(results.json)`
+          files = Dir['*']
+          puts files
+          files = files.keep_if { |f| f != 'results.json' }.map{ |f| f.strip }
+          cmd = "md5sum #{files.join(' ')}"
+          puts cmd
+          filehashes = `#{cmd}`
           filehashes.split("\n").each do |line|
-            filehash, filename = line.strip.split("\t")
-            results[filename] = filehash
+            filehash, filename = line.strip.split("  ")
+          puts "#{filename} - #{filehash}"
+            results << { filename => filehash }
           end
         end
         # compare results to expected
-        expected.each_pair do |key, exp_val|
+        expected.each do |hash|
+          key = hash.keys.first
+          exp_val = hash[key]
+          exist = results.detect { |result| result.key? key }
           match = results.detect { |result| result[key] == exp_val }
-          if match
+          if exist && match
+            puts "PASS: #{key}"
             passed += 1
+          elsif exist
+            puts "WARN: #{key} exists in results but is not exactly the same"
+            warnings += 1
           else
             puts "ERROR: #{key} not found in results"
             puts "expected value: #{exp_val}"
             errors += 1
+          end
         end
       else
         puts "WARN: no test found!"
         warnings += 1
+      end
     end
   end
-end
 
 puts "-" * 25
-puts "tests completed for #{scrapers} scrapers"
+puts "tests completed for #{scrapercount} scrapers"
 puts " - #{passed} passed"
 puts " - #{warnings} warnings"
 puts " - #{errors} errors"
