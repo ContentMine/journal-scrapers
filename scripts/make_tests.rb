ddd4af1d45937e40b5b3b97efa062be1453a37cc
Richard Smith <rds45@cam.ac.uk>
2014-06-22 14:46:30 +0200

    auto test generation script

---
+++ scripts/make_tests.rb
@@ -0,0 +1,86 @@
+#! /usr/bin/env ruby
+
+require 'trollop'
+require 'digest'
+require 'json'
+
+parser = Trollop::Parser.new do
+  banner <<-EOS
+make_tests.rb - ScraperJSON test generator script
+by Richard Smith-Unna <rds45@cam.ac.uk> for ContentMine.org
+
+This script generates a test file from a ScraperJSON scraper definition
+and a list of URLs the scraper applies to.
+
+The test files record what the scraper extracts from each URL so that tests can detect when the scrapers break.
+
+Example use:
+make_tests.rb --definition scraper.json --urls urls.txt
+
+Options:
+EOS
+
+  opt :definition, "Path to ScraperJSON definition",
+      :type => :string,
+      :required => true
+  opt :urls, "File containing a list of 5-10 URLs to test",
+      :type => :string,
+      :required => true
+end
+
+opts = Trollop::with_standard_exception_handling parser do
+  parser.parse ARGV
+  raise Trollop::HelpNeeded if ARGV.empty? # show help screen
+end
+
+urls = File.readlines(opts.urls)
+if urls.length < 5 || urls.length > 10
+  puts "ERROR: you must provide at least 5 and up to 10 URLS"
+end
+
+puts "generating ScraperJSON test files for #{opts.definition}"
+puts "using #{urls.length} urls"
+
+opts.definition = File.expand_path opts.definition
+
+Dir.mkdir('test') unless Dir.exist?('test')
+
+Dir.chdir('test') do
+  testobject = {}
+  # for each URL, run the scraper
+  urls.each_with_index do |url, index|
+    puts "running quickscrape for URL #{url}"
+    results = nil
+    # hash the url
+    hash = Digest::SHA256.hexdigest url
+    # run the scraper
+    cmd = "quickscrape"
+    cmd += " --url #{url}"
+    cmd += " --scraper #{opts.definition}"
+    cmd += " --output #{hash}"
+    cmd += " --loglevel silent"
+    puts `cmd`
+    # load the output
+    Dir.chdir(hash) do
+      results = JSON.load(File.open 'results.json')
+      filehashes = `md5sum !(results.json)`
+      filehashes.split("\n").each do |line|
+        filehash, filename = line.strip.split("\t")
+        results[filename] = filehash
+      end
+    end
+    if (index + 1) < urls.length
+      puts "waiting 15 seconds before next scrape"
+      sleep(15)
+    end
+    # store results for this URL
+    testobject[url] = results
+  end
+
+  # write out the test JSON
+  File.open(File.basename(opts.definition), 'w') do |f|
+    f.write(JSON.pretty_generate(testobject))
+  end
+end
+
+puts "done! Test definition written to test/#{File.basename(opts.definition)}"
